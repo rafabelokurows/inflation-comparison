45e14a8f15a72057cdd732ccf80761991d688dc1
Rafael Belokurows <rafabelokurows@gmail.com>
2023-03-28 23:11:10 +0100

    Update shiny inflation bs4dash.R

--- shiny inflation bs4dash.R
+++ shiny inflation bs4dash.R
@@ -7,8 +7,34 @@ library(dashboardthemes)
 library(ggtext)
 library(tidyverse)
 library(fresh)
-
+library(ggrepel)
 library(bs4Dash)
+
+element_textbox_highlight <- function(..., hi.labels = NULL, hi.fill = NULL,
+                                      hi.col = NULL, hi.box.col = NULL, hi.family = NULL) {
+  structure(
+    c(element_textbox(...),
+      list(hi.labels = hi.labels, hi.fill = hi.fill, hi.col = hi.col, hi.box.col = hi.box.col, hi.family = hi.family)
+    ),
+    class = c("element_textbox_highlight", "element_textbox", "element_text", "element")
+  )
+}
+
+element_grob.element_textbox_highlight <- function(element, label = "", ...) {
+  if (label %in% element$hi.labels) {
+    element$fill <- element$hi.fill %||% element$fill
+    element$colour <- element$hi.col %||% element$colour
+    element$box.colour <- element$hi.box.col %||% element$box.colour
+    element$family <- element$hi.family %||% element$family
+  }
+  NextMethod()
+}
+
+x_mid <- mean(c(max(df2$median_income, na.rm = TRUE), 
+                min(df2$median_income, na.rm = TRUE)))
+
+y_mid <- mean(c(max(df2$value, na.rm = TRUE), 
+                min(df2$value, na.rm = TRUE)))
 
 mytheme <- create_theme(
   adminlte_color(
